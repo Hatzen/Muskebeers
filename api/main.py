cd820625221127ceb6c601f9cb3c48a5a44ca349
Michael Fujarski <michael.fujarski@uni-muenster.de>
2021-09-24 16:03:32 +0200

    First Push with basic server setup.

---
+++ api/main.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/lightning-talk.sqlite"
+create_db = not os.path.isfile("/tmp/lightning-talk.sqlite") # If the File does not exist, the schema needs to be created
+db = SQLAlchemy(app)
+
+
+# Table of Content
+@app.route('/')
+def index():
+    attr = {
+        "content": []
+    }
+    return render_template("index.html.j2", **attr)
+
+if create_db:
+    print("Creating DB")
+    db.create_all()
+
+# Run
+if __name__ == "__main__":
+    kwargs = {
+        'host': '127.0.0.1',
+        'port': 5000
+    }
+    app.run(**kwargs)
